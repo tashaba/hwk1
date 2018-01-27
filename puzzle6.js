bcf860a0f0fba3cea5cc67ce2372199e950fc06a
tashaba <35852644+tashaba@users.noreply.github.com>
2018-01-26 16:08:19 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,22 @@
+right();
+function A () {
+if (getColor ()== "Red") {
+up();
+  up();
+  up();
+} else {
+  down();
+       down();
+  down();
+}}
+function B(){
+  if (getColor () == "Blue") { 
+right();
+up(); }
+else { 
+  left();
+  down();
+}}
+A();
+B();
+  
