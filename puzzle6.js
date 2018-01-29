db083f1e374a0400bf9620804aec1eb0d7c8fa26
tashaba <35852644+tashaba@users.noreply.github.com>
2018-01-29 09:30:35 -0800

    Update puzzle6.js

--- puzzle6.js
+++ puzzle6.js
@@ -1,22 +1,23 @@
 right();
-function A () {
-if (getColor ()== "Red") {
-up();
+function a () {
+  if (getColor () == "red") 
+  { up();
   up();
   up();
-} else {
-  down();
-       down();
-  down();
-}}
-function B(){
-  if (getColor () == "Blue") { 
-right();
-up(); }
-else { 
-  left();
-  down();
-}}
-A();
-B();
-  
+  }
+  else {down();
+        down();
+        down(); 
+       }} 
+function b () {
+if (getColor () == "blue") 
+  { right();
+  up();
+  }
+  else {left();
+        down();
+        
+       }} 
+a();
+b();
+up(); 
