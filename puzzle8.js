c1f7d9d5915643acad7e3c4b9afef86f347d77eb
tashaba <35852644+tashaba@users.noreply.github.com>
2018-01-29 10:05:29 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,37 @@
+
+function down7 () {
+  {down ();
+   down();
+   down();
+   down();
+   down();
+   down();
+   down();
+  
+  }}
+  function right2 () {
+    {right();
+     right();
+    }}
+    
+    function up7 () {
+    {up ();
+     up();
+     up();
+     up();
+     up();
+     up();
+     up();
+     
+    }}
+    down();
+    down7();
+    right2();
+    up7();
+right2();
+down7();
+right2();
+up7();
+right2();
+down7();
+right2();
